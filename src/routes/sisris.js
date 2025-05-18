41f398dd45b37e33534a3122266a78bc0bdd5f72
cesarlunava <a01254603@tec.mx>
2025-05-17 21:44:58 -0700

    Fcheck

--- src/routes/sisris.js
+++ src/routes/sisris.js
@@ -13,21 +13,16 @@ router.post('/add', async (req, res) => {
     await pool.query('UPDATE relays SET state = ? WHERE controller_id = "MEGA2560/2025"', [onoff]);
     req.flash('success', 'Se ha registrado el comando correctamente');
     console.log('Comando ingresado:', onoff);
-    res.render('link/list');
   } catch (err) {
     console.error('Error al insertar comando:', err);
     req.flash('error', 'Hubo un problema al registrar el comando');
   }
 });
 
-router.get('/list', (req, res) => {
-  res.render('links/list');
-});
-
 router.get('/api-relay-state', async (req, res) => {
     try{
-      const [estado] = await pool.query('SELECT state FROM comandos WHERE controller_id = "MEGA2560/2025"');
-      console.log('Estados enviado: ', estado[0]);
+      const estado = await pool.query('SELECT state FROM relays WHERE controller_id = "MEGA2560/2025"');
+      console.log('Estados enviado: ', estado);
       res.json(estado[0])
     } catch(err) {
       console.error('Error al consultar estado: ', err);
